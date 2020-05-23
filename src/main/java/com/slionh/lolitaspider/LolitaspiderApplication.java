e4c844a7c43f34914db1e668885eb2ad272d2c0d
huyungui <841619348@qq.com>
2020-05-23 18:17:31 +0800

    first commit

---
+++ src/main/java/com/slionh/lolitaspider/LolitaspiderApplication.java
@@ -0,0 +1,13 @@
+package com.slionh.lolitaspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolitaspiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolitaspiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slionh/lolitaspider/LolitaspiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slionh.lolitaspider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolitaspiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
